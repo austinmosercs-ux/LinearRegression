3672598b4e4e1351a03a918b78eec712c5c22c54
Austin Moser <austinmosercs@gmail.com>
2026-03-08 10:39:28 -0500

    Rename "Predictions" to "Values of y" for clarity in output display

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ print("w0 =", round(b_w0, 6))
 print("w1 =", round(b_w1, 6))
 print("Iterations:", b_iter)
 print("Equation: y =", round(b_w0, 6), "+", round(b_w1, 6), "* x")
-print("Predictions:")
+print("Values of y:")
 for x_val in [6, -120, 120]:
     y_pred = b_w0 + b_w1 * x_val
     print(f"  x = {x_val}: y = {round(y_pred, 6)}")
@@ -99,7 +99,8 @@ print("w0 =", round(s_w0, 6))
 print("w1 =", round(s_w1, 6))
 print("Iterations:", s_iter)
 print("Equation: y =", round(s_w0, 6), "+", round(s_w1, 6), "* x")
-print("Predictions:")
+print("Values of y:")
 for x_val in [6, -120, 120]:
     y_pred = s_w0 + s_w1 * x_val
     print(f"  x = {x_val}: y = {round(y_pred, 6)}")
+    
